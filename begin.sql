16a70f5fe39f9cef866d539ec2759736e178fa3b
Peter Smatana <petersmatana@gmail.com>
2017-09-10 11:30:38 +0200

    zacinam s pg/pl-sql

---
+++ begin.sql
@@ -0,0 +1,13 @@
+﻿-- pokud fce existuje, dropnu si ji
+drop function if exists public.hello_world();
+
+-- fce
+create function hello_world() returns void as $$
+begin
+end; $$
+language plpgsql;
+
+
+
+
+
